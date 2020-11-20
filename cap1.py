ab3c14ef1b1cd35bcf2fa360fcc276b7a24f4004
Jônatas da Silva <jonatasdasilvasantos@outlook.com>
2020-11-20 17:53:39 -0300

    algoritmo em construção
    reprodução do algoritmo do livro.

--- cap1.py
+++ cap1.py
@@ -1,35 +1,84 @@
-graph = ["1": {
-            "p": 20,
-            "v": -10000000,
-            "l": -10000000,
-            "adjacentes": [1, 3]
+#!/usr/bin/python3
+# imports
+import sys
+
+graph = [{1: {
+            "p": 3,
+            "peso": 3,
+            "v": -1,
+            "adj": [(2, 12), (3, 5), (4, 10)]
         },
-        "2": {
+        2: {
             "p": 10,
-            "v": -10000000,
-            "l": -10000000,
-            "adjacentes": [1, 3]
+            "peso": 10,
+            "v": -1,
+            "adj": [(5, 4), (6, 5)]
         },
-        "3": {
-            "p": 15,
-            "v": -10000000,
-            "l": -10000000,
-            "adjacentes": [1, 2]
-        }
-]
+        3: {
+            "p": 10,
+            "peso": 10,
+            "v": -1,
+            "adj": [(9, 3), (10, 2)]
+        },
+        5: {
+            "p": 10,
+            "peso": 10,
+            "v": -1,
+            "adj": [(7, 4), (8, 5)]
+        },
+}]
+
+V = []
+
+# camputa menos pesso dos arcos.
+def p(v):
+    p = sys.maxsize
+    for x in graph:
+        if x is v:
+            for (no, peso)in v["adj"]:
+                if peso < p:
+                    p = peso
+    return p
+
+def caminhosMinimos(s):
+    k = 0
+    for v in graph:
+        for (no, peso) in v["adj"]:
+            if k == peso != s:
+                k = peso - s
+            else:
+                if peso > s:
+                    k = s
+                elif peso < s:
+                    k = peso
+    return k
+
+# função para cálculo do peso desde a raiz até o nó
+# calcula os menores caminhos baseado nos pesos das arestas
+# Isso aqui da o retorno do vetor V.
+def bfs():
+    return True
+
+# escolhe o maior valor de dentro co vetor V.
+def vMax ():
+    return 0
+
+# min{V(s),p(t)}
+def calculaTMP(): 
+    return 0
 
 def fco(graph):
     Q = []
-    V = ["s": -10000000, "t": -10000000]
     tmp = 0
     l = 1
+    gamma = 1.0
     for vertex in graph:
-        vertex[p] = -1000000000
-        V = vertex[p] - grauMinimo()
-        Q.push(vertex)
+        vertex["p"] = -1
+        V.append(vertex["p"] - gamma)
+        Q.append(vertex)
 
-    while (!Q):
-        if (vertex.p - grauMinimo() == vMax()):
+    while (not Q):
+        if (vertex.p - gamma == vMax()):
             s = Q.pop()
         if vertex.p is null:
             L = l
@@ -37,8 +86,8 @@ def fco(graph):
             vertex[v] = vertex.p
 
     for v in graph:
-        for t in v[adjacentes]:
-            if v[v] > t[v]:
+        for t in v["adj"]:
+            if v["v"] > t["v"]:
                 tmp = calculaTMP() # min{V(s),p(t)}
                 if tmp > t[v]:
                     t[l] = v[l]
@@ -47,7 +96,7 @@ def fco(graph):
 
 
 def main():
-    fco(graph);
+    fco(graph)
 
 if __name__ == "__main__":
         main()
