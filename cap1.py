8c4d97617ff0b422cb1f71ae6e182fb6fda59863
Jônatas da Silva <jonatasdasilvasantos@outlook.com>
2020-11-20 23:53:14 -0300

    codigo final 1

--- cap1.py
+++ cap1.py
@@ -1,34 +1,93 @@
 #!/usr/bin/python3
 # imports
 import sys
+import math
 
-graph = [{1: {
-            "p": 3,
-            "peso": 3,
-            "v": -1,
+graph = {1: {
+            "d": [],
+            "l": 0,
+            "p": 0,
+            "peso": 0,
+            "v": 0,
             "adj": [(2, 12), (3, 5), (4, 10)]
         },
         2: {
-            "p": 10,
+            "d": [],
+            "l": 0,
+            "p": 0,
             "peso": 10,
             "v": -1,
             "adj": [(5, 4), (6, 5)]
         },
         3: {
-            "p": 10,
+            "d": [],
+            "l": 0,
+            "p": 0,
             "peso": 10,
             "v": -1,
             "adj": [(9, 3), (10, 2)]
         },
-        5: {
-            "p": 10,
+        4: {
+            "d": [],
+            "l": 0,
+            "p": 0,
             "peso": 10,
             "v": -1,
             "adj": [(7, 4), (8, 5)]
         },
-}]
+        5: {
+            "d": [],
+            "l": 0,
+            "p": 0,
+            "peso": 10,
+            "v": -1,
+            "adj": []
+        },
+        6: {
+            "d": [],
+            "l": 0,
+            "p": 0,
+            "peso": 10,
+            "v": -1,
+            "adj": []
+        },
+        7: {
+            "d": [],
+            "l": 0,
+            "p": 0,
+            "peso": 10,
+            "v": -1,
+            "adj": []
+        },
+        8: {
+            "d": [],
+            "l": 0,
+            "p": 0,
+            "peso": 10,
+            "v": -1,
+            "adj": []
+        },
+        9: {
+            "d": [],
+            "l": 0,
+            "p": 0,
+            "peso": 10,
+            "v": -1,
+            "adj": []
+        },
+        10: {
+            "d": [],
+            "l": 0,
+            "p": 0,
+            "peso": 10,
+            "v": -1,
+            "adj": []
+        },
+    }
 
+# mapa de valores de Caminhos ótimos do grafo 
 V = []
+P = {} #floresta de caminhos ótimos
 
 # camputa menos pesso dos arcos.
 def p(v):
@@ -40,6 +99,7 @@ def p(v):
                     p = peso
     return p
 
+# detecta caminhos mínimos
 def caminhosMinimos(s):
     k = 0
     for v in graph:
@@ -53,20 +113,64 @@ def caminhosMinimos(s):
                     k = peso
     return k
 
+
+def caminhosOtimos():
+    k = 0
+    for s in graph:
+        if s == 1 and s["v"] == -1:
+            s["v"] = 0
+        for (no, peso) in s["adj"]:
+            graph[no]["v"] = s["p"] + peso
+            graph[no]["d"].append((s,s["d"]+1))
+    for s in graph:
+        V.append((s["v"],s))
+    V.sort(reverse=True)
+    return k
+
 # função para cálculo do peso desde a raiz até o nó
 # calcula os menores caminhos baseado nos pesos das arestas
 # Isso aqui da o retorno do vetor V.
 def bfs():
     return True
 
-# escolhe o maior valor de dentro co vetor V.
+# escolhe o maior valor de dentro do vetor V.
 def vMax ():
-    return 0
+    return V.pop(0)
+
+# distância de s à t
+def d(s, t):
+    dist = graph[t]["d"]
+    k = 0
+    for (pai, distance) in dist:
+        if pai == s:
+            k = distance
+    return distance
+
+def DF():
+    k = 0
+    for v in graph:
+        for (no, peso) in v["adj"]:
+            if peso > k:
+                k = peso
+    return k
+
+def ro (s, t):
+    # Comprimento do maior arco
+    df = DF()
+    ps = (1 / (math.sqrt(2*math.pi*((df/3)**2)) * t["adj"].length)) * (-d(s,t)/2*((df/3)**2))
+    return ps
 
 # min{V(s),p(t)}
-def calculaTMP(): 
-    return 0
+def calculaTMP(s, t):
+    p = 0
+    n = 0
+    for (peso, no) in V:
+        if no == s:
+            n = no
+            p = peso
+    return min(ro(s,t),p)
 
+# Floresta de Caminhos Ótimos
 def fco(graph):
     Q = []
     tmp = 0
@@ -78,21 +182,23 @@ def fco(graph):
         Q.append(vertex)
 
     while (not Q):
-        if (vertex.p - gamma == vMax()):
-            s = Q.pop()
-        if vertex.p is null:
-            L = l
+        no = vMax()
+        s = Q.pop(no[1])
+        if s not in P:
+            graph[s]["l"] = l
             l += 1
-            vertex[v] = vertex.p
+            for (k, w) in V:
+                if w == s:
+                    k = ro(s)
 
     for v in graph:
-        for t in v["adj"]:
-            if v["v"] > t["v"]:
-                tmp = calculaTMP() # min{V(s),p(t)}
-                if tmp > t[v]:
-                    t[l] = v[l]
-                    t[p] = v[p]
-                    t[v] = tmp # atualizar a posição de t em Q
+        for (t, peso) in v["adj"]:
+            if v["v"] > graph[t]["v"]:
+                tmp = calculaTMP(v, t) # min{V(s),p(t)}
+                if tmp > graph[t]["v"]:
+                    graph[t]["l"] = v["l"]
+                    graph[t]["p"] = ro(v, t)
+                    graph[t]["v"] = tmp # atualizar a posição de t em Q
 
 
 def main():
